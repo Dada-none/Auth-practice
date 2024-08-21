f2def3e4b183049f19a18fad6a0eed610e7256d5
강다영 <gadin8631@naver.com>
2024-08-21 17:20:10 +0900

    ✨feat[#1]: 기본 기능 구현(미사용 클래스 삭제)

--- src/main/java/com/dada/auth_practice/presentation/request/LoginRequest.java
+++
@@ -1,8 +0,0 @@
-package com.dada.auth_practice.presentation.request;
-
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginRequest(@NotBlank String client_id,
-                           @NotBlank String redirect_uri,
-                           @NotBlank String response_type) { }
